e61e706cdf4527fc0f416763e9d44506a71ab1bd
axvii6 <amd.1427@icloud.com>
2026-05-06 20:35:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
@@ -10,5 +10,5 @@ def home():
 def love():
     return open("love.html", encoding="utf-8").read()
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
